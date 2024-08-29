0c3b8c1a33351f7f734563eb14ce723ae3b99702
Sachin Goyal <sachingoyal9274@gmail.com>
2024-08-29 21:54:38 +0530

    tt

--- currentprogram.cpp
+++ currentprogram.cpp
@@ -1,10 +1,58 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-class Solution
-{
+class Solution {
+public:
+    int changer(vector<int>& coins, int amount,int size,int coin,int count)
+    {
+        if(amount<coins[0])
+        {
+            return -1;
+        }
+        if((amount%coin)==0)
+        {
+            amount=amount-coin;
+            if(amount==0)
+            {
+                return count+1;
+            }
+            return changer(coins,amount,size,coin,count+1); 
+        }
+        size--;
+        return changer(coins,amount,size,coins[size-1],count);
+    }
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        sort(coins.begin(),coins.end());
+        if(amount==0)
+        {
+            return 0;
+        }
+        if(amount<coins[0])
+        {
+            return -1;
+        }
+        
+        int size=coins.size();
+        int count=0;
+        count=changer(coins,amount,size,coins[size-1],count); 
+        return count;
+    }
 };
 int main()
 {
-    int n=5;
-    cout<<recursive(n);
+    vector<int>coins={2};
+    int amount=3,size=coins.size();
+    if(amount<coins[0])
+    {
+        cout<<-1;
+        return -1;
+    }
+    if(amount==0)
+    {
+        cout<<0;
+        return 0;
+    }
+    int count=0;
+    cout<<changer(coins,amount,size,coins[size-1],count);
+    
 }
